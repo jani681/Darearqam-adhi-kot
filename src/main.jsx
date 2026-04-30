57983facb56b84c5c8b68c5624adb03ecfac0830
jani681 <jani681@gmail.com>
2026-04-30 09:20:37 +0500

    Remove alert for React loading confirmation
    Removed alert to indicate React is working.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Ye alert bataye ga ke app load ho rahi hai
-alert("React is Working!");
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
