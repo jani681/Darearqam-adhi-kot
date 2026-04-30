4853b6a5e35f65f26c6751fd2e2e4ee9d561c625
jani681 <jani681@gmail.com>
2026-04-30 07:38:06 +0500

    Add alert to indicate React is working

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Ye alert bataye ga ke app load ho rahi hai
+alert("React is Working!");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
