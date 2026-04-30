09a4a08d47ee71146e8a6978b00a0e331cbd501c
jani681 <jani681@gmail.com>
2026-04-30 09:02:12 +0500

    Update Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAt2wsDHuc2LdQOXjXb8rrAibWYFgaAX-c",
-  authDomain: "dar-e-arqam-ali-campus.firebaseapp.com",
-  projectId: "dar-e-arqam-ali-campus",
-  storageBucket: "dar-e-arqam-ali-campus.firebasestorage.app",
-  messagingSenderId: "667117683459",
-  appId: "1:667117683459:web:026bc5240a22e5a41e9f36"
+  apiKey: "AIzaSyCgStZINnijw4gO1bmjcKD--YvzWi_hLwo",
+  authDomain: "dar-e-arqam-adhi-kot.firebaseapp.com",
+  projectId: "dar-e-arqam-adhi-kot",
+  storageBucket: "dar-e-arqam-adhi-kot.firebasestorage.app",
+  messagingSenderId: "305555657601",
+  appId: "1:305555657601:web:1e1b42b5bb812752744e80",
+  measurementId: "G-VT4W1CMFFC"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // Storage connection active
