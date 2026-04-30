0b2953a796b4c5e43e263e6f484574ac85fe1522
jani681 <jani681@gmail.com>
2026-04-30 07:50:16 +0500

    Update Firebase storage bucket URL

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAt2wsDHuc2LdQOXjXb8rrAibWYFgaAX-c",
   authDomain: "dar-e-arqam-ali-campus.firebaseapp.com",
   projectId: "dar-e-arqam-ali-campus",
-  storageBucket: "dar-e-arqam-ali-campus.appspot.com",
+  storageBucket: "dar-e-arqam-ali-campus.firebasestorage.app",
   messagingSenderId: "667117683459",
   appId: "1:667117683459:web:026bc5240a22e5a41e9f36"
 };
