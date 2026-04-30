5c1c7f25df83cef7ecb0c64c4bab85cd9bd01f91
jani681 <jani681@gmail.com>
2026-04-30 07:41:16 +0500

    Fix storageBucket URL in firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -5,10 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyAt2wsDHuc2LdQOXjXb8rrAibWYFgaAX-c",
   authDomain: "dar-e-arqam-ali-campus.firebaseapp.com",
   projectId: "dar-e-arqam-ali-campus",
-  storageBucket: "dar-e-arqam-ali-campus.firebasestorage.app",
+  storageBucket: "dar-e-arqam-ali-campus.appspot.com",
   messagingSenderId: "667117683459",
-  appId: "1:667117683459:web:026bc5240a22e5a41e9f36",
-  measurementId: "G-S4DESWN2TG"
+  appId: "1:667117683459:web:026bc5240a22e5a41e9f36"
 };
 
 const app = initializeApp(firebaseConfig);
